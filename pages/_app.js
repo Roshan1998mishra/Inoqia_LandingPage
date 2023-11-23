46aa8d59c9bc32fced74b23cf843f3d3b967e57f
Praveen Gupta <prajune1996@gmail.com>
2023-11-23 06:40:10 +0530

    removed yarn

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
-import '@/styles/globals.css'
-import'/styles/custom.css'
+import "@/styles/globals.css";
+import "/styles/custom.css";
 import "@fontsource/poppins"; // Defaults to weight 400
 import "@fontsource/poppins/400.css"; // Specify weight
 import "@fontsource/poppins/400-italic.css"; //
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Analytics /> <Component {...pageProps} />
+    </>
+  );
 }

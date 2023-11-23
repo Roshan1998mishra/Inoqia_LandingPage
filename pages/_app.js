4523d5c689fc31212f55d371681a2665ebf2b53b
Praveen Gupta <prajune1996@gmail.com>
2023-11-23 06:42:27 +0530

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import "/styles/custom.css";
 import "@fontsource/poppins"; // Defaults to weight 400
 import "@fontsource/poppins/400.css"; // Specify weight
 import "@fontsource/poppins/400-italic.css"; //
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
   return (
